5ee76549e602776f9d200e58640ac3c08048e1d0
firstmeet <2022742378@qq.com>
2023-10-08 15:04:06 +0800

    1

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "stimulus",
   description:
     "The speed of a single-page web application without having to write any JavaScript.",
+  base: "/stimulus/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
